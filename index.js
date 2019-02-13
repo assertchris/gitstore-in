06b0ebd18379b899135f8930a571d0a05bdc5652
Christopher Pitt <cgpitt@gmail.com>
2019-02-13 13:45:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { h, render, Component, Color, Bold } = require("ink")
+const Link = require("ink-link")
+
+class App extends Component {
+    componentDidMount() {
+        setTimeout(function() {
+            process.exit(0)
+        }, 1000)
+    }
+
+    render() {
+        return h(
+            Color,
+            {
+                whiteBright: true,
+                bgBlue: true,
+            },
+            [
+                "We’re still building this. ",
+                h(
+                    Link,
+                    {
+                        url: "https://twitter.com/gitstoreapp",
+                    },
+                    [h(Bold, undefined, ["Follow us for updates"])],
+                ),
+                ".",
+            ],
+        )
+    }
+}
+
+render(h(App))
